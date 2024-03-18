7bd7b97ce8249d61b1646d8d867cf7038a572663
Igor Garcia <igordoble10@gmail.com>
2024-03-18 20:46:09 +0100

    Kontainer nagusiak BorderLayout kudeatzailea

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
+    }
+    }
+    private static void sortuEtaErakutsiUI() {
+    JFrame frame = new JFrame("Maketazio Kudeatzailea");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(600, 400);}
