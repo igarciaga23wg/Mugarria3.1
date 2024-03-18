01b692fb09bbccf68995bbcce025cfd8ec536d14
Igor Garcia <igordoble10@gmail.com>
2024-03-18 21:11:10 +0100

    Revert "Kontainer nagusiak BorderLayout kudeatzailea"
    This reverts commit ab38827fddf15c5409de8fd8fbf48ccb2c429d18.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,2 @@
 public class Main {
-    public static void main(String[] args)
-    {
-
-    }
-    }
-    private static void sortuEtaErakutsiUI() {
-    JFrame frame = new JFrame("Maketazio Kudeatzailea");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(600, 400);}
+    public static void main(String[] args) {
